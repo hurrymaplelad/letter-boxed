1069eca3e21ed963aa5dbdf2334a3831cb5a6f09
Adam Hull <aph@fb.com>
2022-09-30 23:23:37 -0700

    Load filtered dictionary

--- main.mjs
+++ main.mjs
@@ -1,9 +1,13 @@
+import * as fs from 'node:fs';
 import * as readline from 'node:readline/promises';
 import * as process from 'node:process';
 
+const DICTIONARY_PATH = '/usr/share/dict/words';
 
 async function main() {
   const board = await requestBoard();
+  const validWords = await loadDictionary(makeWordFilter(board));
+  console.log(Array.from(validWords));
   process.exit(0);
 }
 
@@ -20,6 +24,29 @@ async function requestBoard() {
     sides.push(input.replaceAll(/\s/g, '').split(''));
   }
   return sides;
+}
+
+function makeWordFilter(board) {
+  const letterRegExp = new RegExp(`^[${board.flat().join()}]+$`, 'i');
+  return word => (
+    // Is long enough
+    word.length > 2 &&
+    // Only uses letters on the board
+    letterRegExp.test(word)
+  );
+}
+
+async function loadDictionary(wordFilter) {
+ const words = new Set();
+ const file = fs.createReadStream(DICTIONARY_PATH);
+ const lines = readline.createInterface({input: file, crlfDelay: Infinity});
+
+ for await (const line of lines) {
+  if (wordFilter(line)) {
+    words.add(line.toLowerCase());
+  }
+ }
+ return words;
 }
 
 main();
